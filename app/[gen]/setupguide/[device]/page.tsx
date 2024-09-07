08c868a9cdb472a2efd8cd60d95f45322feb7ff4
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-09-07 18:45:35 +0300

    fixed typos

--- app/[gen]/setupguide/[device]/page.tsx
+++ app/[gen]/setupguide/[device]/page.tsx
@@ -7,46 +7,50 @@ import LearnProtection from "@/app/components/LearnProtection";
 import Link from "next/link";
 
 function Page({ params }: { params: { device: deviceName; gen: genName } }) {
-  return (
-    <div className="flex flex-col items-center justify-center my-8 gap-8 ">
-      <DeviceCard {...params} />
-      {/* <div className="w-full "><Steps activeGen={params.gen} /></div> */}
-      <div className="flex justify-center items-center">
-        <iframe
-          src="https://player.vimeo.com/video/1003172023?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-          id="player"
-          className="w-full md:w-[500px] lg:w-[750px] h-full md:h-72 lg:h-96 "
-          title="YouTube video player"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <Link
-        href={`/${params.gen}/setupguide/${params.device}/step1`}
-        className="flex items-center justify-center w-auto h-8 md:h-12 px-3 md:px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-bright-green hover:bg-teal-600 focus:shadow-outline focus:outline-none"
-      >
-        <p>إبدأ في الخطوات</p>
-      </Link>
+	const dev = (deviceData[params.gen] as deviceParams[]).find(
+		(value) => value.device === params.device
+	) as deviceParams;
+	return (
+		<div className="flex flex-col items-center justify-center my-8 gap-8 ">
+			<DeviceCard {...params} />
+			{dev.embeddedVideo !== undefined && (
+				<div className="flex justify-center items-center">
+					<iframe
+						{...dev.embeddedVideo}
+						allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+						id="player"
+						className="w-full md:w-[500px] lg:w-[750px] h-full md:h-72 lg:h-96 "
+						title="YouTube video player"
+						allowFullScreen
+					></iframe>
+				</div>
+			)}
+			<Link
+				href={`/${params.gen}/setupguide/${params.device}/step1`}
+				className="flex items-center justify-center w-auto h-8 md:h-12 px-3 md:px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-bright-green hover:bg-teal-600 focus:shadow-outline focus:outline-none"
+			>
+				<p>إبدأ في الخطوات</p>
+			</Link>
 
-      <LearnProtection />
-    </div>
-  );
+			<LearnProtection />
+		</div>
+	);
 }
 
 type DevicePageProps = {
-  params: {
-    device: deviceName;
-    gen: genName;
-  };
+	params: {
+		device: deviceName;
+		gen: genName;
+	};
 };
 
 export default Page;
 
 export async function generateStaticParams() {
-  return gensParams.flatMap((gen: genParam) =>
-    deviceData[gen.gen].map((device: deviceParams) => ({
-      gen: gen.gen,
-      device: device.device,
-    }))
-  );
+	return gensParams.flatMap((gen: genParam) =>
+		deviceData[gen.gen].map((device: deviceParams) => ({
+			gen: gen.gen,
+			device: device.device,
+		}))
+	);
 }
--- app/[gen]/setupguide/page.tsx
+++ app/[gen]/setupguide/page.tsx
@@ -7,32 +7,32 @@ import setupGuideData, { genSetupGuideData } from "./setupGuideData";
 import LearnProtection from "@/app/components/LearnProtection";
 
 function Page({ params }: { params: { gen: genName } }) {
-  const genGuide: genSetupGuideData = setupGuideData[params.gen];
-  return (
-    <div
-      className="flex flex-col items-center justify-center my-8 mx-auto gap-8 "
-      dir="rtl"
-    >
-      <CardGen {...genGuide.genProps} />
-      <h1 className="inline-block px-4 py-2 text-2xl font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
-        بأي جهاز ستبدأ إن شاء الله؟
-      </h1>
-      <p className="text-center md:font-semibold text-sm md:text-lg bg-purple-50 border border-purple-300 text-purple-700 p-4 rounded-lg w-3/4">
-        نوصي بتفعيل الحماية على جميع الأجهزة و عدم الإكتفاء بجهاز واحد فلا يوجد
-        حماية 100% ولكم هي طبقات
-      </p>
-      <div className=" w-4/5 grid grid-cols-2 lg:grid-cols-3 gap-4">
-        {genGuide.devices.map((device, i) => (
-          <DeviceCard {...device} key={i} />
-        ))}
-      </div>
-      <LearnProtection />
-    </div>
-  );
+	const genGuide: genSetupGuideData = setupGuideData[params.gen];
+	return (
+		<div
+			className="flex flex-col items-center justify-center my-8 mx-auto gap-8 "
+			dir="rtl"
+		>
+			<CardGen {...genGuide.genProps} />
+			<h1 className="inline-block px-4 py-2 text-2xl font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
+				بأي جهاز ستبدأ إن شاء الله؟
+			</h1>
+			<p className="text-center md:font-semibold text-sm md:text-lg bg-purple-50 border border-purple-300 text-purple-700 p-4 rounded-lg w-3/4">
+				نوصي بتفعيل الحماية على جميع الأجهزة و عدم الإكتفاء بجهاز واحد
+				فلا يوجد حماية 100% ولكن هي طبقات
+			</p>
+			<div className=" w-4/5 grid grid-cols-2 lg:grid-cols-3 gap-4">
+				{genGuide.devices.map((device, i) => (
+					<DeviceCard {...device} key={i} />
+				))}
+			</div>
+			<LearnProtection />
+		</div>
+	);
 }
 
 export default Page;
 
 export async function generateStaticParams() {
-  return gensParams;
+	return gensParams;
 }
