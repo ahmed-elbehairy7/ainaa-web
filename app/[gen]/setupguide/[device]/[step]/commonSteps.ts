b713e5ef690016e4f961c04fea022a02f05ed6e4
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-03-09 08:23:02 +0200

    some fixes for the about page

--- app/[gen]/setupguide/[device]/[step]/commonSteps.ts
+++ app/[gen]/setupguide/[device]/[step]/commonSteps.ts
@@ -4,4 +4,9 @@ export const supportUsStep: Step[] = [];
 
 export const checkActivationStep: Step[] = [];
 
-export const customerSupportStep: Step[] = [];
+export const customerSupportStep: Step[] = [
+	{
+		data: "قبل أن تبدأ في الخطوات اعلم أنه إن وجدت صعوبة في تنفيذها أو تريد أن نقوم بها حتى لا تعرف الطريقة تواصل معانا عبر الارقام التالية: يمكنك الضغط على أي من المربعات حتى تتواصل معهم على الواتساب",
+		type: "text",
+	},
+];
