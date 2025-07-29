fad9d2c5cb64f400c8f6d7bbcd9db3c5fec3e1bf
Lotr2 <mohamed_anan@aucegypt.edu>
2025-07-29 14:42:50 +0300

    Added black variations of icons to the public folder. Create OtherDeficeButton component that shows a device if it's not soon.

--- app/[gen]/setupguide/[device]/page.tsx
+++ app/[gen]/setupguide/[device]/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import DeviceCard from "@/app/components/DeviceCard";
 import deviceData, { deviceParams } from "./deviceData";
 import { genName } from "@/app/appTypes";
 import { deviceName, genParam, gensParams } from "../../genData";
-import LearnProtection from "@/app/components/LearnProtection";
 import Link from "next/link";
 import Image from "next/image";
-import { googleForm } from "@/importantLinks";
+import OtherDeviceButton from "@/app/components/otherDeviceButton";
+import setupGuideData, { genSetupGuideData } from "../setupGuideData";
 
 const deviceMessages: Record<string, string> = {
   android: "تحميل برنامج الاندرويد",
@@ -18,54 +17,56 @@ function Page({ params }: { params: { device: deviceName; gen: genName } }) {
   const dev = (deviceData[params.gen] as deviceParams[]).find(
     (value) => value.device === params.device
   ) as deviceParams;
-  console.log(dev);
+  const genGuide: genSetupGuideData = setupGuideData[params.gen];
 
   return(
     <div className="flex flex-col items-center mt-8">
       <div className="flex flex-col text-center">
-        <h1 className="font-bold text-4xl tracking-widest">
+        <h1 className="font-bold text-3xl md:text-4xl tracking-widest">
           {deviceMessages[dev.device] ?? "تفعيل الحماية"}
         </h1>
-        <h2 className="text-xl break-normal max-w-2xl tracking-widest mt-2 text-gray-700 ">
+        <h2 className="md:text-xl text-lg break-normal max-w-2xl tracking-widest mt-2 mb-10 text-gray-700 ">
         نوصي بتفعيل الحماية علي جميع الأجهزة وعدم الاكتفاء بجهاز واحد، فلا توجد حماية 100%، ولكنها طبقات متعددة ترفع من مستوي الأمان  
         </h2>
       </div>
-    <div className="flex flex-row-reverse  justify-between items-center gap-10">
-      <div className="text-right max-w-xl">
-        <h1 className="text-3xl font-normal text-gray-700 mb-6 leading-relaxed tracking-widest ">
-        حماية نفسك و أطفالك و أحبابك <br />
-        على بعد خطوات!
-        </h1>
-        <ul className="text-gray-600 text-lg mb-6 space-y-2 pr-2">
-          <li>• ميزة رقم واحد عن التطبيق</li>
-          <li>• ميزة رقم اثنان عن التطبيق</li>
-          <li>• ميزة رقم ثلاثة عن التطبيق</li>
-        </ul>
-        <div className='flex'>
-          <Link
-            href='/support'
-            className='text-white py-4 px-14 rounded-xl transition-all duration bg-gradient-to-l
-            from-[#E0462D] to-[#A73826]
-            hover:from-[#E0462D] hover:to-[#000]
-            '
-          >
-            حمل التطبيق
-          </Link>
+      <div className="flex flex-col lg:mx-0 mx-5">
+        <div className="flex flex-col-reverse md:flex-row-reverse  justify-between items-center gap-10">
+          <div className="text-right max-w-xl">
+            <h1 className="lg:text-3xl text-2xl font-normal text-gray-700 mb-6 leading-relaxed tracking-widest ">
+            حماية نفسك و أطفالك و أحبابك <br />
+            على بعد خطوات!
+            </h1>
+            <ul className="text-gray-700 text-lg mb-6 space-y-2 pr-2 font-extrabold   ">
+              <li>• ميزة رقم واحد عن التطبيق</li>
+              <li>• ميزة رقم اثنان عن التطبيق</li>
+              <li>• ميزة رقم ثلاثة عن التطبيق</li>
+            </ul>
+            <div className='flex'>
+              <Link
+                href='/gen1/setupguide/android'
+                className='text-white py-4 px-14 rounded-xl transition-all duration bg-gradient-to-l
+                from-[#E0462D] to-[#A73826]
+                hover:from-[#E0462D] hover:to-[#000]
+                mb-4 md:mb-0
+                '
+              >
+                حمل التطبيق
+              </Link>
+            </div>
+          </div>
+          <div>
+            <div className='relative md:w-[340px] md:h-[340px] w-[160px] h-[160px] flex justify-center items-center transition-all duration-300 group-hover:scale-105'>
+                <Image
+                  src={require(`@/public/devices/orange-${dev.device}.svg`)}
+                  alt={dev.device}
+                  className='object-contain drop-shadow-md transition-all duration-300 md:ml-24'
+                  width={1000}
+                  height={1000}
+                />
+            </div>
+          </div>
         </div>
-      </div>
-      <div>
-      <div className='relative w-[340px] h-[340px] flex justify-center items-center transition-all duration-300 group-hover:scale-105'>
-           <Image
-            src={require(`@/public/devices/orange-${dev.device}.svg`)}
-            alt={dev.device}
-            className='object-contain drop-shadow-md transition-all duration-300 ml-24'
-            width={1000}
-            height={1000}
-          />
-        </div>
-      </div>
-    </div>
-    <div className='flex justify-end'>
+        <div className='flex md:justify-end sm:justify-start mb-4'>
           <Link
             href='/support'
             className='text-white py-4 px-8 rounded-xl transition-colors'
@@ -77,6 +78,29 @@ function Page({ params }: { params: { device: deviceName; gen: genName } }) {
             خدمة العملاء
           </Link>
         </div>
+        <div className="flex justify-start flex-col">
+            <h1 className="text-xl font-bold border-b-2 border-gray-400 w-fit pl-20 pb-2">
+              أجهزة أخري
+            </h1>
+        </div>
+        <div className="flex flex-row justify-start pt-4 pb-0">
+            {genGuide.devices
+              .filter(
+                (device) =>
+                  !device.soon &&
+                  ['windows', 'router', 'android'].includes(device.device) &&
+                  device.device!==dev.device
+              )
+              .map((device, i,arr) =>
+              {
+                const borderClass = i !== arr.length - 1 ? 'pl-6 border-l border-gray-300 h-auto my-auto' : '';
+                return(
+                  <div key={i} className={borderClass}>
+                <OtherDeviceButton {...device} />
+                 </div>
+              )})}
+        </div>
+      </div>
     </div>
   )
 
