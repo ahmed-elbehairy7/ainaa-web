da62b30d797ccf057d066038381ea7f5d09d99f1
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-08-20 20:58:07 +0300

    added card, wallet options

--- app/_components/SupportUs.tsx
+++ app/_components/SupportUs.tsx
@@ -2,59 +2,63 @@
 import Modal from "../_components/Modal";
 
 type SupportUsProps = {
-  isOpen: boolean;
-  close: boolean;
-  onClose?: () => void;
+	isOpen: boolean;
+	close: boolean;
+	onClose?: () => void;
 };
 export default function SupportUs({ isOpen, onClose, close }: SupportUsProps) {
+	return (
+		<Modal isOpen={isOpen} onClose={onClose} close={close}>
+			<form
+				action="/support_us/pay"
+				method="post"
+				className="flex flex-col items-center justify-center gap-8"
+			>
+				<h2 className="text-2xl sm:text-2xl md:text-3xl font-semibold">
+					ادعمنا
+				</h2>
 
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} close={close}>
-      <form
-        action="/support_us/pay"
-        method="post"
-        className="flex flex-col items-center justify-center gap-8"
-      >
-        <h2 className="text-2xl sm:text-2xl md:text-3xl font-semibold">
-          ادعمنا
-        </h2>
+				{/* Amount */}
+				<div className="flex flex-col justify-center gap-2 w-full">
+					<label
+						htmlFor="amount"
+						className="font-semibold text-base sm:text-lg md:text-xl"
+					>
+						المبلغ (جنيه مصري)
+					</label>
+					<input
+						type="number"
+						name="amount"
+						id="amount"
+						min="10"
+						required
+						autoFocus
+						step="any"
+						className="border border-gray-300 p-2 sm:p-3 md:p-4 rounded-md w-full"
+					/>
+				</div>
 
-        {/* Amount */}
-        <div className="flex flex-col justify-center gap-2 w-full">
-          <label
-            htmlFor="amount"
-            className="font-semibold text-base sm:text-lg md:text-xl"
-          >
-            المبلغ (جنيه مصري)
-          </label>
-          <input
-            type="number"
-            name="amount"
-            id="amount"
-            min="10"
-            required
-            step="any"
-            className="border border-gray-300 p-2 sm:p-3 md:p-4 rounded-md w-full"
-          />
-        </div>
+				{/* Buttons */}
+				<div className="flex flex-col sm:flex-row justify-around w-full items-center gap-4 sm:gap-6 md:gap-8">
+					<button
+						type="submit"
+						name="defaultMethod"
+						value={"card"}
+						className="w-full sm:w-40 p-3  sm:p-4 text-center text-sm sm:text-base md:text-lg rounded-md text-white bg-red-700"
+					>
+						كارت
+					</button>
 
-        {/* Buttons */}
-        <div className="flex flex-col sm:flex-row justify-around w-full items-center gap-4 sm:gap-6 md:gap-8">
-          <button
-            type="submit"
-            className="w-full sm:w-40 p-3  sm:p-4 text-center text-sm sm:text-base md:text-lg rounded-md text-white bg-red-700"
-          >
-            ادعمنا
-          </button>
-
-          {/* <Link
-            href="/"
-            className="w-full sm:w-40 p-3 sm:p-4 text-center text-sm sm:text-base md:text-lg rounded-lg border-2 sm:border-[3px] md:border-4 border-black"
-          >
-            كارت
-          </Link> */}
-        </div>
-      </form>
-    </Modal>
-  );
+					<button
+						type="submit"
+						name="defaultMethod"
+						value={"wallet"}
+						className="w-full sm:w-40 p-3  sm:p-4 text-center text-sm sm:text-base md:text-lg rounded-md text-white bg-red-700"
+					>
+						محفظة
+					</button>
+				</div>
+			</form>
+		</Modal>
+	);
 }
